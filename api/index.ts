ed7a85f2aaf9c6ecf535e8d05d166f32fff7d13e
Kakajaja5089 <kakajaja5089@gmail.com>
2025-02-06 19:59:47 +0100

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,1 +1,7 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import { scrapeAniWorld } from "../src/scraper";
 
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const data = await scrapeAniWorld();
+  return res.json(data);
+}
